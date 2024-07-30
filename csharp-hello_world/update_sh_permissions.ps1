373711044887ba9dd788f4ff72650ea5944bf837
LNCarrion <lestycarrion@gmail.com>
2024-07-30 16:07:09 -0400

    deliting

--- csharp-hello_world/update_sh_permissions.ps1
+++
@@ -1,7 +0,0 @@
-# Get all .sh files tracked by git
-$shFiles = git ls-files "*.sh"
-
-# Update the file mode to executable for each .sh file
-foreach ($file in $shFiles) {
-    git update-index --chmod=+x $file
-}
