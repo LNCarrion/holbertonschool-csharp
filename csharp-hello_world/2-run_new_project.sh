443d381c21059f01be69c2ec4e0cce80835d6c83
LNCarrion <lestycarrion@gmail.com>
2024-08-02 17:12:53 -0400

    file 2

--- csharp-hello_world/2-run_new_project.sh
+++ csharp-hello_world/2-run_new_project.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
-dotnet
+!/usr/bin/env bash
+
+# Check if the directory 2-new_project exists
+if [ ! -d "2-new_project" ]; then
+    # Create a new console project named 2-new_project
+    dotnet new console -n 2-new_project
+fi
+
+# Change the directory to 2-new_project
+cd 2-new_project
+
+# Build the project
+dotnet build
+
+# Run the project
+dotnet run
