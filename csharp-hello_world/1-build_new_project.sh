d39ecf02f5c7c8011720d063f94e5c2922a5299b
LNCarrion <lestycarrion@gmail.com>
2024-08-02 11:08:24 -0400

    addig git index

--- csharp-hello_world/1-build_new_project.sh
+++ csharp-hello_world/1-build_new_project.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-if[-d "1-new_proyect"]; then
-    mkdir -p 1-new_project && cd 1-new_project && dotnet new console && dotnet build
-fi
+
+mkdir -p 1-new_project && cd 1-new_project && dotnet new console && dotnet build
