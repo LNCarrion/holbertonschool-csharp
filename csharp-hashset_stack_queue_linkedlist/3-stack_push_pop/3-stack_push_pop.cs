5eb520992ff4d8ce1fdf9a198842dae7271a4d5b
LNCarrion <lestycarrion@gmail.com>
2024-08-05 19:44:28 -0400

    3 working with stacks

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine($"Number of items: {aStack.Count}");
+
+        if (aStack.Count == 0)
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        else
+        {
+            Console.WriteLine($"Top item: {aStack.Peek()}");
+        }
+
+        bool containsSearch = aStack.Contains(search);
+        Console.WriteLine($"stack contains {search}: {containsSearch}");
+
+        if (containsSearch)
+        {
+            Stack<string> tempStack = new Stack<string>();
+
+            while (aStack.Count > 0)
+            {
+                string item = aStack.Pop();
+                tempStack.Push(item);
+
+                if (item == search)
+                {
+                    break;
+                }
+            }
+
+            while (tempStack.Count > 1)
+            {
+                aStack.Push(tempStack.Pop());
+            }
+        }
+        aStack.Push(newItem);
+
+        return aStack;
+    }
+
+}
