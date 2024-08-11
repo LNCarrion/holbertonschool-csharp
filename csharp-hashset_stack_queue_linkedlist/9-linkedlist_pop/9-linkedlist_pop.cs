40feb5dc6f74f1c69d7ebd057cb7f5ca15cd6b17
LNCarrion <lestycarrion@gmail.com>
2024-08-11 19:20:03 -0400

    complete proyect

---
+++ csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        if (myLList.Count == 0)
+            return 0;
+
+        int lastValue = myLList.First.Value;
+        myLList.RemoveFirst();
+        return lastValue;
+    }
+}
