2c4dbf9175cae6fd2276be4f0de0acaa158061bf
LNCarrion <lestycarrion@gmail.com>
2024-09-04 13:50:38 -0400

    fix framework

--- csharp-inheritance/0-is/0-is.cs
+++ csharp-inheritance/0-is/0-is.cs
@@ -3,7 +3,14 @@ using System.Collections.Generic;
 
 public class Obj
 {
-    public static bool IsOfTypeInt(object obj)
+    /// <summary>
+    /// Determines whether the specified object is of type int.
+    /// </summary>
+    /// <param name="obj">The object to check.</param>
+    /// <returns>
+    ///   <c>true</c> if the specified object is of type int; otherwise, <c>false</c>.
+    /// </returns>
+    public bool IsInt(object obj)
     {
         return obj is int;
     }
