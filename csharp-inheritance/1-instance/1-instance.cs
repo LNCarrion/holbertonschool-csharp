f21c05ce3dc2d23bf1ecde50e513c591101980b0
LNCarrion <lestycarrion@gmail.com>
2024-09-04 14:24:10 -0400

    adding instance

---
+++ csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+/// <summary>
+/// The Obj class.
+/// </summary>
+
+class Obj
+{
+    /// <summary>
+    /// Checks if the given object is an instance of the Array class or 
+    /// a class that inherits from Array.
+    /// </summary>
+    /// <param name="obj">The object to check.</param>
+    /// <returns>True if the object is an instance of Array or inherits from Array, otherwise False.</returns>
+
+    public static bool IsInstanceOfArray(object obj)
+    {
+        return obj is Array;
+    }
+}
