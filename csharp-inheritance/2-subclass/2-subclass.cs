e0e26c147a23c51277b9f1398ace960d4b19ae83
LNCarrion <lestycarrion@gmail.com>
2024-09-04 15:14:22 -0400

    fix code

--- csharp-inheritance/2-subclass/2-subclass.cs
+++ csharp-inheritance/2-subclass/2-subclass.cs
@@ -1,18 +1,16 @@
-﻿using System;
+﻿﻿using System;
 
 /// <summary>
-/// The Obj class.
+/// Class that contains a method to check if a type is a subclass of another type.
 /// </summary>
-
 class Obj
 {
-    // <summary>
-    /// Checks if the specified type is a subclass of the given base type.
-    /// The method returns True only if the specified type is a subclass and not the base class itself.
+    /// <summary>
+    /// Check if a type is a subclass of another type.
     /// </summary>
-    /// <param name="derivedType">The type to check.</param>
-    /// <param name="baseType">The base type to compare against.</param>
-    /// <returns>True if <paramref name="derivedType"/> is a subclass of <paramref name="baseType"/>, otherwise False.</returns>
+    /// <param name="derivedType">The type to check if it is a subclass.</param>
+    /// <param name="baseType">The type to check if it is the base class.</param>
+    /// <returns>True if the derivedType is a subclass of baseType, false otherwise.</returns>
     public static bool IsOnlyASubclass(Type derivedType, Type baseType)
     {
         return derivedType.IsSubclassOf(baseType);
