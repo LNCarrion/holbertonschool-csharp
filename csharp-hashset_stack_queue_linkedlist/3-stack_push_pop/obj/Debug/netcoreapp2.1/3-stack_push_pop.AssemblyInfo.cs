ff4d113efafc8ba9941254ed580f1690ad4729f3
LNCarrion <lestycarrion@gmail.com>
2024-09-05 15:11:59 -0400

    ingeritance override

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/obj/Debug/netcoreapp2.1/3-stack_push_pop.AssemblyInfo.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/obj/Debug/netcoreapp2.1/3-stack_push_pop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("3-stack_push_pop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0e26c147a23c51277b9f1398ace960d4b19ae83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c46b20254c1147cc796ef668ffacf132553cb70")]
 [assembly: System.Reflection.AssemblyProductAttribute("3-stack_push_pop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("3-stack_push_pop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
